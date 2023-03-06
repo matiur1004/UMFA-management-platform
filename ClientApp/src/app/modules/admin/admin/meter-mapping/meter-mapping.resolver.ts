841c081cadafb9bafdd6361482067c776646c60e
Nenad Veselinovic <nenad@umfa.co.za>
2023-03-06 19:56:35 +0100

    implemented access control

--- ClientApp/src/app/modules/admin/admin/meter-mapping/meter-mapping.resolver.ts
+++ ClientApp/src/app/modules/admin/admin/meter-mapping/meter-mapping.resolver.ts
@@ -4,6 +4,7 @@ import {
   RouterStateSnapshot,
   ActivatedRouteSnapshot
 } from '@angular/router';
+import { AuthService } from '@core/auth/auth.service';
 import { BuildingService, UserService } from '@shared/services';
 import { merge, Observable, of } from 'rxjs';
 
@@ -13,10 +14,12 @@ import { merge, Observable, of } from 'rxjs';
 export class MeterMappingResolver implements Resolve<boolean> {
   constructor(
     private _service: BuildingService,
-    private _usrService: UserService
+    private _usrService: UserService,
+    private _authService: AuthService
   ){}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
     return merge(
+      this._usrService.getUser(this._authService.userValue.Id),
       this._service.getBuildingsForUser(this._usrService.userValue.UmfaId)
     );
   }
