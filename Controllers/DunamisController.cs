6de26bee36a7d3370fb4159c71c10b388176a219
Macky McDonald <88426698+Macky-Umfa@users.noreply.github.com>
2023-03-11 00:48:16 +0200

    Final Commit

--- Controllers/DunamisController.cs
+++ Controllers/DunamisController.cs
@@ -1,6 +1,8 @@
 ﻿using ClientPortal.Controllers.Authorization;
 using ClientPortal.Data;
 using ClientPortal.Data.Entities.DunamisEntities;
+using ClientPortal.Data.Entities.PortalEntities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClientPortal.Controllers
 {
@@ -10,20 +12,21 @@ namespace ClientPortal.Controllers
     public class DunamisController : ControllerBase
     {
         private readonly ILogger<DunamisController> _logger;
-        readonly DunamisDBContext _dbService;
+        readonly DunamisDBContext _context;
 
         public DunamisController(ILogger<DunamisController> logger, DunamisDBContext dbService)
         {
-            _dbService = dbService;
+            _context = dbService;
             _logger = logger;
         }
-
+        //SuppliesTo
+        //GET: Dunamis/getAllSuppliesTo
         [HttpGet("getAllSuppliesTo")]
         public async Task<ActionResult<IEnumerable<SuppliesTo>>> GetAllSuppliesTo()
         {
             try
             {
-                return await _dbService.SuppliesTo.ToListAsync();
+                return await _context.SuppliesTo.ToListAsync();
             }
             catch (Exception ex)
             {
@@ -31,5 +34,30 @@ namespace ClientPortal.Controllers
                 return BadRequest($"Error while retrieving SuppliesTo Items: {ex.Message}");
             }
         }
+
+        //LocationType
+        //GET: MappedMeters/getAllLocationTypes
+        [HttpGet("getAllLocationTypes")]
+        public async Task<List<LocationType>> GetAllLocationTypes()
+        {
+            try
+            {
+                var locationTypes = await _context.LocationTypes.FromSqlRaw("SELECT t.name AS SuppliesTo, " +
+               "CASE typ.SupplyType WHEN 0 THEN 'Electricity' WHEN 1 THEN 'Water' WHEN 2 THEN 'Gas' WHEN 3 THEN 'Sewerage' " +
+               "WHEN 4 THEN 'Solar' ELSE 'AdHoc' END AS SupplyType, loc.Name AS LocationName FROM SuppliesTo t " +
+               "JOIN SuppliesToSupplyTypes typ ON (t.Id = typ.SuppliesToId)" +
+               "JOIN SuppliesToSupplyTypesLocations l ON (typ.Id = l.SuppliesToSupplyTypeId)" +
+               "JOIN SupplyToLocations loc ON (l.SupplyToLocationId = loc.Id) ORDER BY 1, 2, 3").ToListAsync();
+                return locationTypes.ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while retrieving SuppliesTo Items: {ex.Message}");
+                return new List<LocationType> { };
+            }
+
+           
+        }
+
     }
 }
