60296025ff5daafeed1d1902c91991203ca2b619
Gerhard-Shad <78953367+Gerhard-Shad@users.noreply.github.com>
2023-03-29 09:32:42 +0100

    updated notifucatino seeding values

--- Migrations/20230308111631_NotificationTypesToDB.cs
+++ Migrations/20230308111631_NotificationTypesToDB.cs
@@ -27,13 +27,12 @@ namespace ClientPortal.Migrations
                new[] { "Name" },
                new object[,]
                {
+                    {"Night flow"},
                     {"Burst Pipe"},
-                    {"UMFA Operator"},
-                    {"Night Flow"},
-                    {"Max Usage Exceeded"},
-                    {"Peak Exceeded"},
                     {"Leak Detection"},
-                    {"Total Exceeded"}
+                    {"Daily Usage Exceeded"},
+                    {"Peak Exceeded"},
+                    {"Average Exceeded"}
                });
         }
 
