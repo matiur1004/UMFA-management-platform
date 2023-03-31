d0d7adc4671d10c8f4f14bbcc95a1d42ceba1f76
Macky McDonald <88426698+Macky-Umfa@users.noreply.github.com>
2023-03-31 14:12:13 +0200

    API Check

--- Controllers/AMRMeterGraphsController.cs
+++ Controllers/AMRMeterGraphsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ClientPortal.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AMRMeterGraphsController : ControllerBase
     {
