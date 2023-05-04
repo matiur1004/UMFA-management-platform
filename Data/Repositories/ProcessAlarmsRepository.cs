4107e90280bcbc4d5ddebbed8f70386910f0fbd3
Gerhard-Shad <78953367+Gerhard-Shad@users.noreply.github.com>
2023-05-04 17:15:29 +0100

    busy with alarm processing

---
+++ Data/Repositories/ProcessAlarmsRepository.cs
@@ -0,0 +1,65 @@
+﻿using ClientPortal.Data.Entities.PortalEntities;
+
+namespace ClientPortal.Data.Repositories
+{
+    public class ProcessAlarmsRepository
+    {
+        private readonly ILogger<ProcessAlarmsRepository> _logger;
+        private readonly PortalDBContext _context;
+
+        public ProcessAlarmsRepository(ILogger<ProcessAlarmsRepository> logger, PortalDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AMRMeterAlarm>> GetAlarmsToProcess()
+        {
+            try
+            {
+                _logger.LogInformation("Retrieving alarms to process from database...");
+                var ret = await _context.AMRMeterAlarms
+                    .Where(ma => ma.LastRunDTM == null || ma.LastRunDTM < DateTime.Now.AddMinutes(-60))
+                    .ToListAsync();
+
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while getting alarms to process: {ex.Message}");
+                throw;
+            }
+        }
+
+        public async Task<bool> ProcessNightFlow(AMRMeterAlarm alarm)
+        {
+            try
+            {
+                alarm.LastRunDTM = DateTime.Now;
+                _context.Update<AMRMeterAlarm>(alarm);
+                if ((await _context.SaveChangesAsync()) == 0 )
+                {
+                    _logger.LogError($"Update of alarm run date failed for alarm : {alarm.AMRMeterAlarmId}");
+                    return false;
+                }
+                
+                var meterSerial = (await _context.AMRMeters.FindAsync(alarm.AMRMeterId)).MeterSerial;
+                DateTime? lastProfileData = (await _context.ScadaProfileData.Where(p => p.SerialNumber == meterSerial)
+                    .Select(p => p.ReadingDate)
+                    .MaxAsync());
+                if (!lastProfileData.HasValue)
+                {
+                    _logger.LogError($"No Profile data for meter {meterSerial}");
+                    return false; 
+                }
+                DateTime lastAlarmData = alarm.LastDataDTM?? DateTime.Now.AddHours(-24);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while processing nightflow alarm {alarm.AMRMeterAlarmId}: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ Services/ProcessAlarmsService.cs
@@ -0,0 +1,64 @@
+﻿using ClientPortal.Data;
+using ClientPortal.Data.Entities.PortalEntities;
+using ClientPortal.Data.Repositories;
+
+namespace ClientPortal.Services
+{
+    public class ProcessAlarmsService
+    {
+        private readonly ILogger<ProcessAlarmsService> _logger;
+        private readonly ProcessAlarmsRepository _repo;
+
+        public ProcessAlarmsService(ILogger<ProcessAlarmsService> logger, ProcessAlarmsRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        public async Task<bool> ProcessAlarms()
+        {
+            try
+            {
+                bool ret = false;
+                _logger.LogInformation("Calling repo for alarms...");
+                var alarms = await _repo.GetAlarmsToProcess();
+                if (alarms == null)
+                {
+                    _logger.LogInformation("No Alarms to process");
+                }
+                else
+                {
+                    var nightFlow = alarms.Where(a => a.AlarmTypeId == 1).ToList();
+                    if (nightFlow != null && nightFlow.Count > 0)
+                        ret = ProcessNightFlowAlarms(nightFlow);
+                }
+
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while Processing alarms: {ex.Message}");
+                throw;
+            }
+        }
+
+        private bool ProcessNightFlowAlarms(List<AMRMeterAlarm> alarms)
+        {
+            try
+            {
+                _logger.LogInformation("Now processing nightflow alarms...");
+                foreach (var alarm in alarms)
+                {
+                    _ = _repo.ProcessNightFlow(alarm).GetAwaiter().GetResult();
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while processing nightflow alarms: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
