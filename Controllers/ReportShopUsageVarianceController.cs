b0d8883e44f80d976001dce2deba20afb0ba93b1
Macky <gerhard.mcdonald@umfa.co.za>
2023-05-18 13:21:40 +0200

    Fixed Types

--- Controllers/ReportShopUsageVarianceController.cs
+++ Controllers/ReportShopUsageVarianceController.cs
@@ -130,9 +130,9 @@ namespace ClientPortal.Controllers
                             dynamic totalsRow = new ExpandoObject();
                             var totalsDictionary = totalsRow as IDictionary<string, object>;
 
-                            totalsDictionary.Add(fieldNames[0], "TOTALS");
-                            totalsDictionary.Add(fieldNames[1], "FOR");
-                            totalsDictionary.Add(fieldNames[2], "GROUP:");
+                            totalsDictionary.Add(fieldNames[0], "TOTALS FOR GROUP:");
+                            totalsDictionary.Add(fieldNames[1], false);
+                            totalsDictionary.Add(fieldNames[2], null);
                             totalsDictionary.Add(fieldNames[3], "");
                             totalsDictionary.Add(fieldNames[4], "");
                             totalsDictionary.Add(fieldNames[5], "");
