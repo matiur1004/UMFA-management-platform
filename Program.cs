cb2e2b5ac0d7076b98569477daf94e8c49c771ae
Gerhard-Shad <78953367+Gerhard-Shad@users.noreply.github.com>
2023-05-19 09:16:21 +0100

    support for linux drawing tool

--- Program.cs
+++ Program.cs
@@ -144,6 +144,7 @@ var app = builder.Build();
 {
     if (!app.Environment.IsDevelopment())
     {
+        DevExpress.Drawing.Internal.DXDrawingEngine.ForceSkia();
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
     }
