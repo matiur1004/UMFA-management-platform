26026e070b43ddc487c4eecb9912ffc641b3fc36
Conrad <x00170725@mytudublin.ie>
2023-06-06 11:12:31 +0100

    Added MappedMeterResponse & initial MappedMeterRepository

---
+++ Data/Repositories/MappedMeterRepository.cs
@@ -0,0 +1,24 @@
+﻿using ClientPortal.Models.ResponseModels;
+
+namespace ClientPortal.Data.Repositories
+{
+    public interface IMappedMeterRepository
+    {
+        public Task<MappedMeterResponse> GetMappedMeters();
+        public Task<MappedMeterResponse> GetMappedMeters(int buildingId);
+    }
+
+
+    public class MappedMeterRepository : IMappedMeterRepository
+    {
+        public Task<MappedMeterResponse> GetMappedMeters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MappedMeterResponse> GetMappedMeters(int buildingId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/ResponseModels/MappedMeterResponse.cs
@@ -0,0 +1,25 @@
+﻿using ClientPortal.Data.Entities.PortalEntities;
+
+namespace ClientPortal.Models.ResponseModels
+{
+    public class MappedMeterResponse
+    {
+        public int BuildingId { get; set; }
+        public string Response { get; set; }
+        public List<MappedMeter> MappedMeters { get; set; }
+
+        public MappedMeterResponse() 
+        {
+            BuildingId = 0;
+            Response = "Initiated";
+            MappedMeters = new List<MappedMeter>();
+        }
+
+        public MappedMeterResponse(int buildingId)
+        {
+            BuildingId = buildingId;
+            Response = $"Initiated for building {buildingId}";
+            MappedMeters = new List<MappedMeter>();
+        }
+    }
+}
