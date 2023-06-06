6cc28a5b80fc12ad74442428ba8dfc47d19fca00
Gerhard-Shad <78953367+Gerhard-Shad@users.noreply.github.com>
2023-06-06 14:44:45 +0100

    migration for sp changes

---
+++ Migrations/20230606134419_POR96ChangedSPs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientPortal.Migrations
+{
+    public partial class POR96ChangedSPs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
