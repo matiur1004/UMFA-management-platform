9e23049cd7fa778645b5be91a99d53607c19c871
Conrad <x00170725@mytudublin.ie>
2023-06-07 15:06:12 +0100

    Updated scada request header & detail controlers

--- Data/Repositories/RepositoryBase.cs
+++ Data/Repositories/RepositoryBase.cs
@@ -1,33 +1,38 @@
-﻿namespace ClientPortal.Data.Repositories
+﻿using System.Linq.Expressions;
+
+namespace ClientPortal.Data.Repositories
 {
     public interface IRepository<TTable>
     {
-        public Task<List<TTable>> GetAll();
-        public Task<TTable> Get(int id);
-        public Task<TTable> Add(TTable entity);
-        public Task<TTable> Update(TTable entity);
-        public Task<TTable> Remove(int id);
+        public Task<List<TTable>> GetAllAsync();
+        public Task<List<TTable>> GetAllAsync(params Expression<Func<TTable, object>>[] includes);
+        public Task<TTable> GetAsync(int id);
+        public Task<TTable> GetAsync(int id, string keyPropertyName, params Expression<Func<TTable, object>>[] includes);
+        public Task<TTable> AddAsync(TTable entity);
+        public Task<TTable> UpdateAsync(TTable entity);
+        public Task<TTable> RemoveAsync(int id);
+        
     }
 
-    public abstract class RepositoryBase<T, TContext> : IRepository<T> where TContext : DbContext where T : class // TODO a model base class would be better
+    public abstract class RepositoryBase<TTable, TContext> : IRepository<TTable> where TContext : DbContext where TTable : class // TODO a model base class would be better
     {
-        private readonly ILogger<RepositoryBase<T, TContext>> _logger;
+        private readonly ILogger<RepositoryBase<TTable, TContext>> _logger;
         private readonly TContext _dbContext;
 
-        protected RepositoryBase(ILogger<RepositoryBase<T, TContext>> logger, TContext dbContext)
+        protected RepositoryBase(ILogger<RepositoryBase<TTable, TContext>> logger, TContext dbContext)
         {
             _logger = logger;
             _dbContext = dbContext;
         }
-        public async Task<T> Add(T entity)
+        public async Task<TTable> AddAsync(TTable entity)
         {
-            _logger.LogInformation($"Adding new {typeof(T).Name}");
+            _logger.LogInformation($"Adding new {typeof(TTable).Name}");
 
-            var tableExists = _dbContext.Model.FindEntityType(typeof(T).Name) != null;
+            var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
 
             if (!tableExists)
             {
-                _logger.LogError($"{typeof(T).Name} Table is Not Found");
+                _logger.LogError($"{typeof(TTable).Name} Table is Not Found");
                 return default;
             }
 
@@ -35,87 +40,173 @@
 
             await _dbContext.SaveChangesAsync();
 
-            _logger.LogInformation($"{typeof(T).Name} Saved Successfully!");
+            _logger.LogInformation($"{typeof(TTable).Name} Saved Successfully!");
 
             return createdModel.Entity;
         }
 
-        public async Task<T> Get(int id)
+        public async Task<TTable> GetAsync(int id)
         {
-            _logger.LogInformation($"Retrieving {typeof(T).Name} with Id: {id}");
+            _logger.LogInformation($"Retrieving {typeof(TTable).Name} with Id: {id}");
 
-            var tableExists = _dbContext.Model.FindEntityType(typeof(T).Name) != null;
+            var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
 
             if (!tableExists)
             {
-                _logger.LogError($"{typeof(T).Name} Table is Not Found");
+                _logger.LogError($"{typeof(TTable).Name} Table is Not Found");
                 return null;
             }
 
-            var entity = await _dbContext.Set<T>().FindAsync(id);
+            var entity = await _dbContext.Set<TTable>().FindAsync(id);
 
             if (entity == null)
             {
-                _logger.LogError($"{typeof(T).Name} with Id: {id} Not Found!");
+                _logger.LogError($"{typeof(TTable).Name} with Id: {id} Not Found!");
                 return null;
             }
 
-            _logger.LogInformation($"{typeof(T).Name} with Id: {id} Found and Returned!");
+            _logger.LogInformation($"{typeof(TTable).Name} with Id: {id} Found and Returned!");
             return entity;
         }
-
-        public async Task<List<T>> GetAll()
+        public async Task<TTable> GetAsync(int id, string keyPropertyName, params Expression<Func<TTable, object>>[] includes)
         {
-            _logger.LogInformation($"Retrieving all {typeof(T).Name}s");
+            _logger.LogInformation($"Retrieving {typeof(TTable).Name} with Id: {id}");
 
-            var tableExists = _dbContext.Model.FindEntityType(typeof(T).Name) != null;
+            var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
 
             if (!tableExists)
             {
-                _logger.LogError($"{typeof(T).Name}s Table is Not Found");
+                _logger.LogError($"{typeof(TTable).Name} Table is Not Found");
                 return null;
             }
 
-            _logger.LogInformation($"All {typeof(T).Name}s returned.");
-            return await _dbContext.Set<T>().ToListAsync();
-        }
+            var query = _dbContext.Set<TTable>().AsQueryable();
 
-        public async Task<T> Remove(int id)
-        {
-            _logger.LogInformation($"Removing {typeof(T).Name} with Id: {id}");
-
-            var tableExists = _dbContext.Model.FindEntityType(typeof(T).Name) != null;
-
-            if (!tableExists)
+            // Apply includes to the query
+            foreach (var include in includes)
             {
-                _logger.LogError($"{typeof(T).Name} Table is Not Found");
+                query = query.Include(include);
+            }
+
+            // Get the key property using reflection
+            var keyProperty = typeof(TTable).GetProperty(keyPropertyName);
+            if (keyProperty == null)
+            {
+                _logger.LogError($"{typeof(TTable).Name} does not have a property with the name {keyPropertyName}");
                 return null;
             }
 
-            var entity = await _dbContext.Set<T>().FindAsync(id);
+            // Create the expression to compare the key property
+            var parameter = Expression.Parameter(typeof(TTable), "e");
+            var propertyAccess = Expression.MakeMemberAccess(parameter, keyProperty);
+            var equality = Expression.Equal(propertyAccess, Expression.Constant(id));
+            var lambda = Expression.Lambda<Func<TTable, bool>>(equality, parameter);
+
+            var entity = await query.FirstOrDefaultAsync(lambda);
 
             if (entity == null)
             {
-                _logger.LogError($"{typeof(T).Name} with Id: {id} Not Found!");
+                _logger.LogError($"{typeof(TTable).Name} with Id: {id} Not Found!");
                 return null;
             }
 
-            _dbContext.Set<T>().Remove(entity);
-            await _dbContext.SaveChangesAsync();
-
-            _logger.LogInformation($"{typeof(T).Name} with Id: {id} removed successfully");
+            _logger.LogInformation($"{typeof(TTable).Name} with Id: {id} Found and Returned!");
             return entity;
         }
 
-        public async Task<T> Update(T entity)
-        {
-            _logger.LogInformation($"Updating {typeof(T).Name}");
 
-            var tableExists = _dbContext.Model.FindEntityType(typeof(T).Name) != null;
+        public async Task<List<TTable>> GetAllAsync()
+        {
+            _logger.LogInformation($"Retrieving all {typeof(TTable).Name}s");
+
+            var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
 
             if (!tableExists)
             {
-                _logger.LogError($"{typeof(T).Name} Table is Not Found");
+                _logger.LogError($"{typeof(TTable).Name}s Table is Not Found");
+                return null;
+            }
+
+            _logger.LogInformation($"All {typeof(TTable).Name}s returned.");
+            return await _dbContext.Set<TTable>().ToListAsync();
+        }
+
+        public async Task<List<TTable>> GetAllAsync(params Expression<Func<TTable, object>>[] includes)
+        {
+            _logger.LogInformation($"Retrieving all {typeof(TTable).Name}s");
+
+            var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
+
+            if (!tableExists)
+            {
+                _logger.LogError($"{typeof(TTable).Name} Table is Not Found");
+                return null;
+            }
+
+            var query = _dbContext.Set<TTable>().AsQueryable();
+
+            // Apply includes to the query
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            var entities = await query.ToListAsync();
+
+            if (entities == null)
+            {
+                _logger.LogError($"Could not retrieve from {typeof(TTable).Name}");
+                return null;
+            }
+
+            _logger.LogInformation($"All {typeof(TTable).Name}s returned.");
+            return entities;
+        }
+
+        public async Task<TTable> RemoveAsync(int id)
+        {
+            try
+            {
+                _logger.LogInformation($"Removing {typeof(TTable).Name} with Id: {id}");
+
+                var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
+
+                if (!tableExists)
+                {
+                    _logger.LogError($"{typeof(TTable).Name} Table is Not Found");
+                    return null;
+                }
+
+                var entity = await _dbContext.Set<TTable>().FindAsync(id);
+
+                if (entity == null)
+                {
+                    _logger.LogError($"{typeof(TTable).Name} with Id: {id} Not Found!");
+                    return null;
+                }
+
+                _dbContext.Set<TTable>().Remove(entity);
+                await _dbContext.SaveChangesAsync();
+
+                _logger.LogInformation($"{typeof(TTable).Name} with Id: {id} removed successfully");
+                return entity;
+            }
+            catch (DbUpdateException e)
+            {
+                _logger.LogError($"Could not delete from {typeof(TTable).Name} Id: {id} Error: {e.Message}");
+                return null;
+            }
+        }
+
+        public async Task<TTable> UpdateAsync(TTable entity)
+        {
+            _logger.LogInformation($"Updating {typeof(TTable).Name}");
+
+            var tableExists = _dbContext.Model.FindEntityType(typeof(TTable)) != null;
+
+            if (!tableExists)
+            {
+                _logger.LogError($"{typeof(TTable).Name} Table is Not Found");
                 return null;
             }
 
@@ -127,12 +218,13 @@
             }
             catch (DbUpdateConcurrencyException)
             {
-                _logger.LogError($"{typeof(T).Name} could not be updated!");
+                _logger.LogError($"{typeof(TTable).Name} could not be updated!");
                 return null;
             }
 
-            _logger.LogInformation($"{typeof(T).Name} updated successfully");
+            _logger.LogInformation($"{typeof(TTable).Name} updated successfully");
             return entity;
         }
+
     }
 }
---
+++ Services/ScadaRequestService.cs
@@ -0,0 +1,104 @@
+﻿using ClientPortal.Data.Entities.PortalEntities;
+using ClientPortal.Data.Repositories;
+using ClientPortal.Models.RequestModels;
+
+namespace ClientPortal.Services
+{
+    public interface IScadaRequestService
+    {
+        public Task<List<ScadaRequestHeader>> GetScadaRequestHeadersAsync();
+        public Task<ScadaRequestHeader> GetScadaRequestHeaderAsync(int id);
+        public Task<ScadaRequestHeader> UpdateScadaRequestHeaderAsync(ScadaRequestHeaderUpdateRequest scadaRequestHeader);
+        public Task<ScadaRequestHeader> AddScadaRequestHeaderAsync(ScadaRequestHeaderRequest scadaRequestHeader);
+        public Task<ScadaRequestHeader> RemoveScadaRequestHeaderAsync(int id);
+
+        public Task<List<ScadaRequestDetail>> GetScadaRequestDetailsAsync();
+        public Task<ScadaRequestDetail> GetScadaRequestDetailAsync(int id);
+        public Task<ScadaRequestDetail> UpdateScadaRequestDetailAsync(ScadaRequestDetailUpdateRequest scadaRequestDetail);
+        public Task<ScadaRequestDetail> AddScadaRequestDetailAsync(ScadaRequestDetailRequest scadaRequestDetail);
+        public Task<ScadaRequestDetail> RemoveScadaRequestDetailAsync(int id);
+    }
+    public class ScadaRequestService : IScadaRequestService
+    {
+        private readonly ILogger<ScadaRequestService> _logger;
+        private readonly IScadaRequestRepository<ScadaRequestHeader> _scadaRequestHeaderRepo;
+        private readonly IScadaRequestRepository<ScadaRequestDetail> _scadaRequestDetailRepo;
+
+        public ScadaRequestService(ILogger<ScadaRequestService> logger,
+            IScadaRequestRepository<ScadaRequestHeader> scadaRequestHeaderRepo,
+            IScadaRequestRepository<ScadaRequestDetail> scadaRequestDetailRepo)
+        {
+            _logger = logger;
+            _scadaRequestHeaderRepo = scadaRequestHeaderRepo;
+            _scadaRequestDetailRepo = scadaRequestDetailRepo;
+        }
+
+        #region Headers
+        public async Task<ScadaRequestHeader> GetScadaRequestHeaderAsync(int id)
+        {
+            return await _scadaRequestHeaderRepo.GetAsync(id, nameof(ScadaRequestHeader.Id), x => x.ScadaRequestDetails);
+        }
+
+        public async Task<List<ScadaRequestHeader>> GetScadaRequestHeadersAsync()
+        {
+            return await _scadaRequestHeaderRepo.GetAllAsync(x => x.ScadaRequestDetails);
+        }
+
+        public async Task<ScadaRequestHeader> UpdateScadaRequestHeaderAsync(ScadaRequestHeaderUpdateRequest scadaRequestHeader)
+        {
+            var header = await _scadaRequestHeaderRepo.GetAsync(scadaRequestHeader.Id, nameof(scadaRequestHeader.Id), x => x.ScadaRequestDetails);
+
+            header.Map(scadaRequestHeader);
+
+            return await _scadaRequestHeaderRepo.UpdateAsync(header);
+        }
+
+        public async Task<ScadaRequestHeader> AddScadaRequestHeaderAsync(ScadaRequestHeaderRequest scadaRequestHeader)
+        {
+            var header = new ScadaRequestHeader();
+            header.Map(scadaRequestHeader);
+
+            return await _scadaRequestHeaderRepo.AddAsync(header);
+        }
+
+        public async Task<ScadaRequestHeader> RemoveScadaRequestHeaderAsync(int id)
+        {
+            return await _scadaRequestHeaderRepo.RemoveAsync(id);
+        }
+        #endregion
+
+        #region Details
+        public async Task<ScadaRequestDetail> GetScadaRequestDetailAsync(int id)
+        {
+            return await _scadaRequestDetailRepo.GetAsync(id);
+        }
+
+        public async Task<List<ScadaRequestDetail>> GetScadaRequestDetailsAsync()
+        {
+            return await _scadaRequestDetailRepo.GetAllAsync();
+        }
+
+        public async Task<ScadaRequestDetail> UpdateScadaRequestDetailAsync(ScadaRequestDetailUpdateRequest scadaRequestDetail)
+        {
+            var detail = await _scadaRequestDetailRepo.GetAsync(scadaRequestDetail.Id, nameof(scadaRequestDetail.Id), x => x.AmrMeter, x => x.AmrScadaUser, x => x.Header);
+
+            detail.Map(scadaRequestDetail);
+
+            return await _scadaRequestDetailRepo.UpdateAsync(detail);
+        }
+
+        public async Task<ScadaRequestDetail> AddScadaRequestDetailAsync(ScadaRequestDetailRequest scadaRequestDetail)
+        {
+            var detail = new ScadaRequestDetail();
+            detail.Map(scadaRequestDetail);
+
+            return await _scadaRequestDetailRepo.AddAsync(detail);
+        }
+
+        public async Task<ScadaRequestDetail> RemoveScadaRequestDetailAsync(int id)
+        {
+            return await _scadaRequestDetailRepo.RemoveAsync(id);
+        }
+        #endregion
+    }
+}
