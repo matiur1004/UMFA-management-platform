bdc70c5938e28c8e3e77cc6ff1cc210bbc756bdc
Nenad Veselinovic <nenad@umfa.co.za>
2023-06-13 23:50:40 +0200

    shop usage report updated

--- ClientApp/src/app/shared/services/dx-report-service.ts
+++ ClientApp/src/app/shared/services/dx-report-service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root' })
 export class DXReportService {
-  private _shopUsageVariance: BehaviorSubject<any> = new BehaviorSubject([]);
+  private _shopUsageVariance: BehaviorSubject<any> = new BehaviorSubject(null);
 
   constructor(
     private buildingService: BuildingService,
@@ -251,14 +251,14 @@ export class DXReportService {
   }
 
   getReportDataForShop() {
-    // this.SUVParams = {
-    //   "BuildingId": 2983,
-    //   "StartPeriodId": 172047,
-    //   "ToPeriodId": 172047,
-    //   "AllTenants": 1
-    // }
-    const url = `${CONFIG.apiURL}/ReportShopUsageVariance/getReportDataNoPivotWithAverageAndVariance`;
-    return this.http.post<any>(url, this.SUVParams, { withCredentials: true })
+    const url = `${CONFIG.apiURL}/Reports/ShopUsageVarianceReport`;
+    const queryParams = '?' +
+      Object.keys(this.SUVParams)
+        .map(key => {
+          return `${key}=${encodeURIComponent(this.SUVParams[key])}`;
+        })
+        .join('&');
+    return this.http.get<any>(url + queryParams, { withCredentials: true })
       .pipe(
           catchError(err => this.catchErrors(err)),
           tap(m => {
