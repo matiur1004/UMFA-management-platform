32fff2016916eef5ba8f627f24690d328135e0da
Conrad <x00170725@mytudublin.ie>
2023-06-13 16:53:43 +0100

    Added shopCostVariance Endpoint, fixed some bugs

---
+++ Models/ResponseModels/ShopCostVarianceReportResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace ClientPortal.Models.ResponseModels
+{
+    public class ShopCostVarianceReportResponse
+    {
+        public List<TenantShopInvoiceCostGrouping> TenantShopInvoiceGroupings { get; set; } = new List<TenantShopInvoiceCostGrouping>();
+        public List<PeriodTotalCostDetails> Totals { get; set; } = new List<PeriodTotalCostDetails>();
+    }
+
+    public class TenantShopInvoiceCostGrouping
+    {
+        public string Group { get; set; }
+        public int GroupId { get; set; }
+        public string ShopId { get; set; }
+        public string Shop { get; set; }
+        public string Tenants { get; set; }
+        public DateTime OccDTM { get; set; }
+        public decimal? Average { get; set; }
+        public string? Variance { get; set; }
+        public List<PeriodCostDetail> PeriodCostDetails { get; set; } = new List<PeriodCostDetail>();
+    }
+
+    public class PeriodTotalCostDetails
+    {
+        public string GroupName { get; set; }
+        public int GroupId { get; set; }
+        public List<PeriodCostDetail> PeriodCostDetails { get; set; } = new List<PeriodCostDetail>();
+        public decimal? Average { get; set; }
+        public string Variance { get; set; }
+    }
+
+    public class PeriodCostDetail
+    {
+        public int PeriodId { get; set; }
+        public string PeriodName { get; set; }
+        public decimal? Cost { get; set; }
+    }
+}
