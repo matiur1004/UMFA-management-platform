80748ffb27df7e4088d6cd59f7369510da6b5447
Conrad <x00170725@mytudublin.ie>
2023-06-20 12:58:25 +0100

    Added services and repos required to get rid of dbcontext in AlarmTriggeredController

--- Data/Repositories/RepositoryBase.cs
+++ Data/Repositories/RepositoryBase.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System.Linq.Expressions;
 
 namespace ClientPortal.Data.Repositories
@@ -14,7 +16,8 @@ namespace ClientPortal.Data.Repositories
         public Task<TTable> AddAsync(TTable entity);
         public Task<TTable> UpdateAsync(TTable entity);
         public Task<TTable> RemoveAsync(int id);
-        
+        public Task<T> RunStoredProcedureAsync<T, TArgumentClass>(string procedure, TArgumentClass? args = default) where T : new();
+        public Task RunStoredProcedureAsync<TArgumentClass>(string procedure, TArgumentClass? args = default);
     }
 
     public abstract class RepositoryBase<TTable, TContext> : IRepository<TTable> where TContext : DbContext where TTable : class // TODO a model base class would be better
@@ -27,6 +30,103 @@ namespace ClientPortal.Data.Repositories
             _logger = logger;
             _dbContext = dbContext;
         }
+        public async Task<T> RunStoredProcedureAsync<T, TArgumentClass>(string procedure, TArgumentClass? args = default) where T : new()
+        {
+            var connection = _dbContext.Database.GetDbConnection();
+            if (connection.State == System.Data.ConnectionState.Closed)
+                await connection.OpenAsync();
+
+            string commandText = $"exec {procedure}";
+
+            // add arguments
+            if (args is not null)
+            {
+                var argumentProperties = typeof(TArgumentClass).GetProperties();
+
+                var arguments = string.Join(", ", argumentProperties.Select(property =>
+                {
+                    var value = property.GetValue(args);
+                    if (value is int || value is bool)
+                    {
+                        int intValue = (value is bool bit) ? (bit ? 1 : 0) : Convert.ToInt32(value);
+                        return $"@{property.Name} = {intValue}";
+                    }
+                    else
+                    {
+                        return $"@{property.Name} = '{value}'";
+                    }
+                }));
+
+                commandText += $" {arguments}";
+            }
+
+            using (var results = await connection.QueryMultipleAsync(commandText))
+            {
+                if (results == null)
+                {
+                    _logger.LogError($"Not time to run yet...");
+                    return default(T);
+                }
+
+                var combinedResult = new T();
+
+                foreach (var property in typeof(T).GetProperties())
+                {
+                    if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                    {
+                        var resultType = property.PropertyType.GetGenericArguments()[0];
+                        var result = await results.ReadAsync(resultType);
+                        var resultList = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(result), property.PropertyType);
+                        property.SetValue(combinedResult, resultList);
+                    }
+                }
+
+                return combinedResult;
+            }
+        }
+
+        public async Task RunStoredProcedureAsync<TArgumentClass>(string procedure, TArgumentClass? args = default)
+        {
+            var connection = _dbContext.Database.GetDbConnection();
+            if (connection.State == System.Data.ConnectionState.Closed)
+                await connection.OpenAsync();
+
+            string commandText = $"exec {procedure}";
+
+            // add arguments
+            if (args is not null)
+            {
+                var argumentProperties = typeof(TArgumentClass).GetProperties();
+
+                var arguments = string.Join(", ", argumentProperties.Select(property =>
+                {
+                    var value = property.GetValue(args);
+                    if (value is int || value is bool)
+                    {
+                        int intValue = (value is bool bit) ? (bit ? 1 : 0) : Convert.ToInt32(value);
+                        return $"@{property.Name} = {intValue}";
+                    }
+                    else
+                    {
+                        return $"@{property.Name} = '{value}'";
+                    }
+                }));
+
+                commandText += $" {arguments}";
+            }
+
+            using (var results = await connection.QueryMultipleAsync(commandText))
+            {
+                if (results == null)
+                {
+                    _logger.LogError($"Not time to run yet...");
+                    return;
+                }
+
+                return;
+            }
+        }
+
         public async Task<TTable> AddAsync(TTable entity)
         {
             _logger.LogInformation($"Adding new {typeof(TTable).Name}");
---
+++ Services/AMRMeterTriggeredAlarmService.cs
@@ -0,0 +1,40 @@
+﻿using ClientPortal.Data.Entities.PortalEntities;
+using ClientPortal.Data.Repositories;
+using ClientPortal.Models.RequestModels;
+using ClientPortal.Models.ResponseModels;
+
+namespace ClientPortal.Services
+{
+    public interface IAMRMeterTriggeredAlarmService
+    {
+        Task<AMRMeterTriggeredAlarm> AcknowledgeAlarmAsync(AMRMeterTriggeredAlarmAcknowledgeRequest acknowledgement, int id);
+        Task<AlarmTriggeredSpResponse> GetTriggeredAlarms(int amrMeterTriggeredAlarmId);
+    }
+    public class AMRMeterTriggeredAlarmService : IAMRMeterTriggeredAlarmService
+    {
+        private readonly ILogger<AMRMeterTriggeredAlarmService> _logger;
+        private readonly IAMRMeterTriggeredAlarmRepository _repository;
+        
+        public AMRMeterTriggeredAlarmService(ILogger<AMRMeterTriggeredAlarmService> logger, IAMRMeterTriggeredAlarmRepository repository) 
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public async Task<AMRMeterTriggeredAlarm> AcknowledgeAlarmAsync(AMRMeterTriggeredAlarmAcknowledgeRequest acknowledgement, int id) 
+        {
+            var alarm = await _repository.GetAsync(id);
+            alarm.Acknowledged = (bool)acknowledgement.Acknowledged!;
+
+            var updatedAlarm = await _repository.UpdateAsync(alarm);
+
+            return updatedAlarm;
+        }
+
+        public async Task<AlarmTriggeredSpResponse> GetTriggeredAlarms(int amrMeterTriggeredAlarmId)
+        {
+            return await _repository.RunStoredProcedureAsync<AlarmTriggeredSpResponse, AMRMeterTriggeredAlarmSpRequest>("spGetTriggeredAlarm", new AMRMeterTriggeredAlarmSpRequest { AlarmTriggerId = amrMeterTriggeredAlarmId});
+        }
+
+    }
+}
