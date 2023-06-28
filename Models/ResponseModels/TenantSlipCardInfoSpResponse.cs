ab4127edf53dd1ca6851b37f4b686a364b1505f5
Conrad <x00170725@mytudublin.ie>
2023-06-28 11:01:16 +0100

    New endpoint for tenant slip card info

---
+++ Models/ResponseModels/TenantSlipCardInfoSpResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace ClientPortal.Models.ResponseModels
+{
+    public class TenantSlipCardInfoSpResponse
+    {
+        public List<TenantSlipCardInfo> TenantSlipCardInfos { get; set; }
+    }
+
+    public class TenantSlipCardInfo
+    {
+        public int Tenants { get; set; }
+        public int Shops { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
