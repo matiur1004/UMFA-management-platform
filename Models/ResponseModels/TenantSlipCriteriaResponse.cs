bd5b8125d88374d17ea8e9e01d118b61849ef8fd
Conrad <x00170725@mytudublin.ie>
2023-06-28 13:03:13 +0100

    Small change to tenantslipcriteria response

--- Models/ResponseModels/TenantSlipCriteriaResponse.cs
+++ Models/ResponseModels/TenantSlipCriteriaResponse.cs
@@ -5,14 +5,14 @@
         public List<TenantSlipCriteriaPeriodList> PeriodLists { get; set; }
         public List<TenantSlipCriteriaReportType> ReportTypes { get; set; }
         public List<TenantSlipCriteriaFileFormat> FileFormats { get; set; }
-        public TenantSlipCriteriaFileName? FileName { get; set; }
+        public string? FileName { get; set; }
 
         public TenantSlipCriteriaResponse(TenantSlipCriteriaSpResponse response)
         {
             PeriodLists = response.PeriodLists;
             ReportTypes = response.ReportTypes;
             FileFormats = response.FileFormats;
-            FileName = response.FileNames.FirstOrDefault();
+            FileName = response.FileNames.FirstOrDefault()?.FileName;
         }
     }
 }
