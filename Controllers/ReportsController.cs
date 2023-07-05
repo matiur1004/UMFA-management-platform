502cea77f7e4d9a0eb2bd1183b7c2e879719df4a
Conrad <x00170725@mytudublin.ie>
2023-07-05 22:19:27 +0100

    Added ordering to archived reports response

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -111,7 +111,7 @@ namespace ClientPortal.Controllers
                     return Problem("Something went wrong");
                 }
 
-                return archives;
+                return archives.OrderByDescending(a => a.CreatedDateTime).ToList();
             }
             catch(Exception e)
             {
