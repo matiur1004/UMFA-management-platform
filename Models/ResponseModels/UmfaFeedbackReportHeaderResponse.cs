d4e50216beaca8788bc975fc4e8eb4ad546ca5ee
Conrad <x00170725@mytudublin.ie>
2023-10-01 15:42:05 +0100

    Added period and building name to feedbackreportheader response and table

--- Models/ResponseModels/UmfaFeedbackReportHeaderResponse.cs
+++ Models/ResponseModels/UmfaFeedbackReportHeaderResponse.cs
@@ -5,6 +5,7 @@
         public string Portfolio { get; set; }
         public string Property { get; set; }
         public double GLA { get; set; }
+        public string PeriodName { get; set; }
         public DateTime PeriodFrom { get; set; }
         public DateTime PeriodTo { get; set; }
     }
