e27f03dbca2d5341df423864f879a4043b496411
Gerhard-Shad <78953367+Gerhard-Shad@users.noreply.github.com>
2023-11-27 15:00:16 +0000

    changed sp spGetSmartServices

--- Migrations/20231127144221_AddSpBuildingAlarms.cs
+++ Migrations/20231127144221_AddSpBuildingAlarms.cs
@@ -8,7 +8,7 @@ namespace ClientPortal.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("CREATE proc [dbo].[spGetAlarmsPerBuilding]\r\n\r\nAS\r\n\r\nselect\r\nb.PartnerId, b.Partner, b.UmfaId as BuildingId, b.Name as BuildingName\r\n, count(distinct ta.AMRMeterTriggeredAlarmId) as NoOfAlarms\r\nfrom\r\nBuildings b \r\njoin AMRMeters m on (b.UmfaId = m.BuildingId)\r\njoin AMRMeterAlarms ma on (m.Id = ma.AMRMeterId and ma.Active = 1)\r\njoin AMRMeterTriggeredAlarms ta on (ma.AMRMeterAlarmId = ta.AMRMeterAlarmId and ta.Acknowledged = 0 and ta.Active = 1)\r\ngroup by\r\nb.PartnerId, b.Partner, b.UmfaId, b.Name\r\n");
+            migrationBuilder.Sql("CREATE OR ALTER PROC [dbo].[spGetAlarmsPerBuilding]\r\n\r\nAS\r\n\r\nselect\r\nb.PartnerId, b.Partner, b.UmfaId as BuildingId, b.Name as BuildingName\r\n, count(distinct ta.AMRMeterTriggeredAlarmId) as NoOfAlarms\r\nfrom\r\nBuildings b \r\njoin AMRMeters m on (b.UmfaId = m.BuildingId)\r\njoin AMRMeterAlarms ma on (m.Id = ma.AMRMeterId and ma.Active = 1)\r\njoin AMRMeterTriggeredAlarms ta on (ma.AMRMeterAlarmId = ta.AMRMeterAlarmId and ta.Acknowledged = 0 and ta.Active = 1)\r\ngroup by\r\nb.PartnerId, b.Partner, b.UmfaId, b.Name\r\n");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
